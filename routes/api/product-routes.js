c0e2aedecf52bf6d78f255fb7b9c9fd79bda2fed
Sean <seanms418@gmail.com>
2022-02-23 17:34:23 +1100

    changed the routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,8 +9,9 @@ router.get('/', async (req, res) => {
   try { 
     const productData = await Product.findAll({
     include: [
-      { model: Category, attributes: ['category_name']}, 
-      { model: Tag, attributes: ['tag_name']}],
+      { model: Category }, 
+      { model: Tag }
+    ],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -25,8 +26,8 @@ router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
       include: [
-        { model: Category, attributes: ['category_name']}, 
-        { model: Tag, attributes: ['tag_name']}],
+        { model: Category }, 
+        { model: Tag }],
       });
     res.status(200).json(productData)
   } catch (err) {
