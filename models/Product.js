1f676232af106f60a0fafd90638c51110c7cea00
Sean <seanms418@gmail.com>
2022-02-23 13:27:18 +1100

    added the columns to the product model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,41 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+        msg: "Must be a decimal",
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      default: 10,
+      validate: {
+        isNumeric: true,
+        msg: "Must be an Interger",
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false
+      },
+    },
   },
   {
     sequelize,
