d88ac19d695ef4c1b8279c8ed718c81da775407e
Sean <seanms418@gmail.com>
2022-02-28 15:16:19 +1100

    fixed put route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -65,7 +65,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', async (req, res) => {
+router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
